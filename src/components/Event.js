697861ce8da69b4ac2e3b2983991ba1907d4c8ef
AndersonTsaiTW <AndersonTsaiTW@gmail.com>
2024-08-01 03:34:00 -0400

    Styling for the elements

--- src/components/Event.js
+++ src/components/Event.js
@@ -6,11 +6,11 @@ const Event = ({ event }) => {
   const [detailsVisible, setDetailsVisible] = useState(false);
 
   return (
-    <li>
+    <li className='event'>
       <h1>{event.summary}</h1>
       <p>Start Time: {event.created}</p>
       <p>Location: {event.location}</p>
-      <button onClick={() => setDetailsVisible(!detailsVisible)}>
+      <button className='details-btn' onClick={() => setDetailsVisible(!detailsVisible)}>
         {detailsVisible ? 'hide details' : 'show details'}
       </button>
       {detailsVisible && (
